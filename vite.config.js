ad4a4e265e5ffe15adb2c6a353fb57eff7c3ca26
Brett A McCall <brettmccall@gmail.com>
2025-08-12 20:22:21 -0400

    new react site, works locally

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    include: "**/*.{jsx,js}",
+  })],
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.(jsx?|tsx?)$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   build: {
     outDir: 'static/js',
     rollupOptions: {
